0de4c52ccb61b35bb349236aaab46628ce36b9de
Joshua Geng <swegengjoshua@gmail.com>
2024-10-07 01:21:32 -0600

    Added NavBar

--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,15 @@
 import React from "react";
+import Nav from './components/Nav'
+
+
+function App(){
+    return(
+        <div className="App">
+      <Nav /> 
+      <div className="game">
+      </div>
+    </div>
+    );
+}
+
+export default App;
---
+++ src/app/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../assets/App.css';
+
+function Nav(){
+    return(
+        <nav className = "navbar">
+            <h1>Battleship Game</h1>
+            <button>Restart</button>
+        </nav>
+    );
+}
+
+export default Nav;
